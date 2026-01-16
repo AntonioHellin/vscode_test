ad3d12658cf33a1ead7a07a8ca7846d0e975b144
AntonioHellin <antonio.hellinh@gmail.com>
2026-01-16 14:57:02 +0100

    Add Styleissues class demonstrating naming convention issues

---
+++ Styleissues.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Styleissues {
+    public void BadNameMethod() {
+        System.out.println("This method name does not follow camelCase convention.");
+
+        double diVariableLocaldd = 0;
+        System.out.println(diVariableLocaldd);
+        int MiVariableLocal = 0;
+        System.out.println(MiVariableLocal);
+        int miVariableTest = 0;
+        System.out.println(miVariableTest);
+    }
+}
